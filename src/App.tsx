87960bd87bc70cc02b5c70a1ffe0e6e38001f9ef
nimrab <nimrab.0@gmail.com>
2023-10-31 22:02:14 +0300

    add routing, lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {Counter} from "../public/components/Counter";
+import React, {Suspense} from 'react';
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            fesdfsef
-            <Counter/>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>}></Route>
+                    <Route path={'/about'} element={<AboutPageAsync/>}></Route>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
